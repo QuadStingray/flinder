54226f41750e324124ead2b5693c8ba48b06f8f0
tom <tom@sfxcode.com>
2019-06-01 11:56:55 +0200

    applicationState

---
+++ lib/state/ApplicationState.dart
@@ -0,0 +1,41 @@
+import 'package:flinder/model/event.dart';
+import 'package:flinder/model/user.dart';
+import 'package:flutter/foundation.dart';
+
+class ApplicationState with ChangeNotifier {
+
+  ApplicationState();
+
+  bool _isFetchingEventData = false;
+  bool _isFetchingUserData = false;
+
+  Future<void> fetchEventData() async {
+    _isFetchingEventData = true;
+    notifyListeners();
+
+   // todo
+
+    _isFetchingEventData = false;
+    notifyListeners();
+  }
+
+  Future<void> fetchUserData() async {
+    _isFetchingUserData = true;
+    notifyListeners();
+
+    // todo
+
+    _isFetchingUserData = false;
+    notifyListeners();
+  }
+
+  List<User> getUserList() {
+
+  }
+
+  List<Event> getEventList() {
+
+  }
+
+
+  }
