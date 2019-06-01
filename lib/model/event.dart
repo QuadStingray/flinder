98c2fac01bf34f93738a0860a94d6fab5b9f17d8
tom <tom@sfxcode.com>
2019-06-01 12:56:00 +0200

    events sample from API

--- lib/model/event.dart
+++ lib/model/event.dart
@@ -48,4 +48,29 @@ class Event {
       'tags': tags
     };
   }
+
+  List<Event> sampleEvents = [
+    Event(
+        id: "1234",
+        title: "Awesome Flutter",
+        location: LatLng(49.78951644897461, 9.980087280273438),
+        startTime: DateTime.parse("2019-07-23 18:00:00Z"),
+        endTime: DateTime.parse("2019-07-23 23:00:00Z"),
+        description:
+            "Awesome Flutter in Würzburg",
+        source: "meetup",
+        externalId: "261446017",
+        tags: ["google", "flutter"]),
+    Event(
+        id: "45678",
+        title: "Flutter by Example",
+        location: LatLng(49.7975959777832, 9.928215026855469),
+        startTime: DateTime.parse("2019-08-21 19:00:00Z"),
+        endTime: DateTime.parse("2019-08-21 22:00:00Z"),
+        description:
+        "Flutter Samples explained.",
+        source: "meetup",
+        externalId: "25243523",
+        tags: ["flutter", "coding"])
+  ];
 }
